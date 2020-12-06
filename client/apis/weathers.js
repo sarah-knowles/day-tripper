4cf4efb4e2f470e64b89948ca2a2f4d06ff80421
sarah-knowles <sjk339@gmail.com>
2020-12-06 15:49:35 +1300

    geolocation

--- client/apis/weathers.js
+++ client/apis/weathers.js
@@ -1,16 +1,25 @@
 import request from 'superagent'
 
-const rootUrl = '/api/v1'
-//const googleURL = '?key=AIzaSyCQp5vJIqasej3dl1FEkkjmvuXYzT2hk9w&cx=b03569b1cf26aa1b9&q='
+const rootUrl = '/api/v1/weathers'
+//const googleURL = '?key=[OUR_API_KEY]&cx=b03569b1cf26aa1b9&q='
 
 
 export function getWeathers() {
-  return request.get(rootUrl + '/weathers')
+  let userLocation = {}
+  navigator.geolocation.getCurrentPosition(function(position) {
+    //console.log("Latitude is :", position.coords.latitude);
+    //console.log("Longitude is :", position.coords.longitude);
+    userLocation = {latt: position.coords.latitude, long: position.coords.longitude}
+    //console.log(userLocation)
+     return userLocation
+  });
+ 
+  return request
+  .get(rootUrl + (userLocation.latt, userLocation.long))
     .then(res => {
       return res.body
     })
   }
-
 
 export function getSearch(weatherCode) {
 //console.log("code", weatherCode )
@@ -24,12 +33,13 @@ export function getSearch(weatherCode) {
 // } else console.log('Broken')
 
 
+
 //function to build a url using the URL plus search term
 return request
-  .get('https://www.googleapis.com/customsearch/v1?key=AIzaSyCQp5vJIqasej3dl1FEkkjmvuXYzT2hk9w&gl=NZ&cx=b03569b1cf26aa1b9&q=' + 'beach') 
+  .get('https://www.googleapis.com/customsearch/v1?key=AIzaSyCQp5vJIqasej3dl1FEkkjmvuXYzT2hk9w&gl=NZ&near=Takapuna&cx=b03569b1cf26aa1b9&q=' + 'cinema') 
     .then(res => {
-      console.log(res)
-      return res.body
+      console.log(res.req.url)
+      return res.req.url
     })
 
   }
--- client/components/BackgroundVideo.jsx
+++ client/components/BackgroundVideo.jsx
@@ -4,24 +4,6 @@ import { connect } from 'react-redux'
 import { fetchWeathers } from '../actions/index'
 
 export class BackgroundVideo extends React.Component {
-// const BackgroundVideo = () => {
-//     const videoSource = 'videos/sunny.mp4'
-//     return (
-//         <div className={classes.Container} >
-//             <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
-//                 <source src={videoSource} type="video/mp4" />
-//                 Your browser does not support this video.
-//             </video>
-
-//             <div className={classes.Content}>
-//                 <div className={classes.SubContent} >
-//                 <h1>today is...</h1>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 
 componentDidMount() {
     this.props.dispatch(fetchWeathers())
