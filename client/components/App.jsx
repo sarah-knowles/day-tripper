4dd1062324ac73ef2078afd6a8be3b3b008032de
sarah-knowles <sjk339@gmail.com>
2020-12-08 15:10:28 +1300

    working clock

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import Weather from './Weather'
 import Trips from './Trips'
 import BackGroundVideo from './BackgroundVideo'
 import Map from './Map'
+import Nav from './Nav'
 import Footer from './Footer'
 import { getSearch } from '../apis/weathers'
 // import { loadClient, execute } from '../apis/google'
@@ -24,6 +25,8 @@ export class App extends React.Component {
     return (
       <>
         <div>
+          <Nav />
+          <br/>
           <BackGroundVideo />
           <Weather />
         </div>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-export class Nav extends React.Component {
-  render () {
+export default class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date()
+
+    });
+  }
+  render() {
+    const h = this.state.time.getHours()
+    const m = this.state.time.getMinutes()
+
     return (
-      <p className='clock'></p>
-    )
+      <p className="clock">
+        {h % 12}:{(m < 10 ? '0' + m : m)} {h < 12 ? 'am' : 'pm'}
+      </p>
+    );
   }
 }
